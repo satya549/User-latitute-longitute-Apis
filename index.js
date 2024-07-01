c226d056f9d8d98e7874adfe3214c83a0b433bfe
Satyadev Maurya <in.satyadev@gmail.com>
2024-07-01 23:26:23 +0530

    db connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import  express  from "express";
+import db from "./src/db/connection"
 
 const app = express();
 
---
+++ src/db/connection.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1:27017/machinetest',)
+  .then(() => {
+    console.log('DB connected successfully');
+  })
+  .catch((e) => {
+    console.log('DB connection failed', e);
+  });
+
+export default mongoose;
