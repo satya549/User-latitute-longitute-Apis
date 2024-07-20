1d239f707571049f3598c576901c3d98847a6670
Satyadev Maurya <in.satyadev@gmail.com>
2024-07-20 22:19:15 +0530

    token validator in middleware

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,5 +1,19 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const tokenValidator = (req, res, next) =>{
+export const tokenValidator = (req, res, next) => {
+  const token = req.headers["authorization"];
+  try {
+    if (!token) throw new Error("Token is required in header");
+    const authToken = token.split(" ");
 
-}
+    const decoded = jwt.verify(authToken[1], secretKey);
+
+    next();
+  } catch (error) {
+    return res.json({
+        status_code: 401,
+        success: false,
+        message: error.message,
+      });;
+  }
+};
